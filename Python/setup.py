9027473f5f45ad2eea1f7a405aa266275c695217
De-Proggremmer <ilya.mitrofanov07@gmail.com>
2025-12-19 04:48:35 +0300

    Delete Python directory

--- Python/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, Extension
-
-setup(
-    name='standDev',               # Имя пакета
-    version='1.0',                 # Версия пакета
-    ext_modules=[Extension('standDev', ['Source.cpp'])]  # Указание расширения на C/C++, которое будет собрано из файла Source.cpp
-)
